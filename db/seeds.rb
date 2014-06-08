e55ea2f029d054d3270c244ba867d542d3a4dd8c
Kevin McMorrow <kmcm07+github@gmail.com>
2014-06-08 23:07:01 +0100

    Add an articles controller and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create(title: 'Welcome!',
+               text: 'This is my first blog post in my new Ruby on Rails blog.')
