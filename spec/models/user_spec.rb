2345915f342b91ac4e99f385cc69e0f589bd8fed
Kevin McMorrow <kmcm07+github@gmail.com>
2014-06-16 19:20:08 +0100

    Add tests for user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   before do
     @user = User.new(email: "kevin@example.org",
-                     password: "secret",
-                     password_confirmation: "secret")
+                     password: "secretpass",
+                     password_confirmation: "secretpass")
   end
 
   subject { @user }
+
+  it { should be_valid }
 
   it { should respond_to :email }
   it { should respond_to :password }
@@ -16,6 +18,52 @@ RSpec.describe User, :type => :model do
 
   describe "when email is not present" do
     before { @user.email = '' }
+
+    it { should_not be_valid }
+  end
+
+  describe "when password is too short" do
+    before do
+      @user.password = 'test123'
+      @user.password_confirmation = 'test123'
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation doesn't match password" do
+    before { @user.password_confirmation = 'wrong' }
+
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[userexamplecom user.example.com user@example
+        example.com user@example@example.com user@example+example.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@example.com user+test@example.com user@example.ie]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when email already exists" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
 
     it { should_not be_valid }
   end
