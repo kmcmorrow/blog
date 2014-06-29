1b660f13900d29de43a86fd09d71623442a19e52
Kevin McMorrow <kmcm07+github@gmail.com>
2014-06-29 14:28:32 +0100

    Add tests for sessions_helper

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -11,13 +11,62 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SessionsHelper, :type => :helper do
-  describe "sign in user" do
-    let(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
 
+  describe "sign in user" do
+    it "sets the current user" do
+      helper.sign_in user
+      
+      expect(helper.current_user).to eq(user)
+    end
+
+    it "sets the remember token" do
+      expect { helper.sign_in user }.to change(user, :remember_token)
+    end
+
+    it "stores the remember token in the session" do
+      helper.sign_in user
+
+      expect(session[:remember_token]).to_not be_blank
+    end
+  end
+
+  describe "after signing in" do
     before { helper.sign_in user }
 
-    it "signs in the user" do
+    it "is signed_in?" do
       expect(helper).to be_signed_in
+    end
+  end
+
+  describe "when @current_user is set" do
+    before { helper.sign_in user }
+    
+    describe "current_user" do
+      it "returns the the current user" do
+        expect(helper.current_user).to eq(user)
+      end
+    end
+  end
+
+  describe "when @current_user is not set" do
+    describe "when remember_token is in session" do
+      before do
+        helper.sign_in user
+        helper.current_user = nil
+      end
+      
+      it "returns the user" do
+        expect(helper.current_user).to eq(user)
+      end
+    end
+
+    describe  "when remember_token is not in session" do
+      before { session.delete :remember_token }
+      
+      it "returns nil" do
+        expect(helper.current_user).to be_nil
+      end
     end
   end
 end
