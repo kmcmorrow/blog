35ea97ae372a58dd38818cc4756536b72b807a0b
Kevin McMorrow <kmcm07+github@gmail.com>
2014-07-24 21:59:00 +0100

    Add specs for Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Comment.new }
+  it { should respond_to :name }
+  it { should respond_to :text }
+
+  describe "validation" do
+    describe "has valid name and text" do
+      it "should be valid" do
+        comment = Comment.new(name: 'Guest', text: 'Hello')
+        expect(comment).to be_valid
+      end
+    end
+
+    describe "has a blank name" do
+      it "should not be valid" do
+        comment = Comment.new(name: '', text: 'test')
+        expect(comment).to_not be_valid
+      end
+    end
+
+    describe "has a blank text" do
+      it "should not be valid" do
+        comment = Comment.new(name: 'test', text: '')
+        expect(comment).to_not be_valid
+      end
+    end
+  end
 end
