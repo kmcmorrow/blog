d8b906b1cc5a602d06844f3e079d37fce1ab3342
Kevin McMorrow <kmcm07+github@gmail.com>
2014-08-13 14:00:55 +0100

    Add link to create a new category

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   resources :sessions, only: [:new, :create, :destroy]
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:index, :show, :new, :create]
   
   root 'articles#index'
 
--- spec/views/categories/index.html.erb_spec.rb
+++ spec/views/categories/index.html.erb_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
+include SessionsHelper
 
 RSpec.describe "categories/index.html.erb", :type => :view do
+  before { assign(:categories, []) }
+  
   describe "when categories exist" do
     before do
       @categories = []
@@ -11,6 +14,22 @@ RSpec.describe "categories/index.html.erb", :type => :view do
     it "displays links to each category" do
       render
       5.times { |n| expect(rendered).to have_link("Category {n}", @categories[n]) }
+    end
+  end
+
+  describe "when logged in" do
+    before { sign_in(FactoryGirl::create(:user)) }
+    
+    it "should show add category link" do
+      render
+      expect(rendered).to have_link('Add category', new_category_path)
+    end
+  end
+
+  describe "when not logged in" do
+    it "should not show add category link" do
+      render
+      expect(rendered).to_not have_link('Add category')
     end
   end
 end
