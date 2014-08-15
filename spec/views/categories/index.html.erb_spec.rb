916b74c3f974720eddcf858228e8dd88a85c647d
Kevin McMorrow <kmcm07+github@gmail.com>
2014-08-15 14:00:30 +0100

    Fix string interpolation bug

--- spec/views/categories/index.html.erb_spec.rb
+++ spec/views/categories/index.html.erb_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe "categories/index.html.erb", :type => :view do
   describe "when categories exist" do
     before do
       @categories = []
-      5.times { |n| @categories << FactoryGirl::create(:category, name: "Category {n}") }
+      5.times { |n| @categories << FactoryGirl::create(:category, name: "Category #{n}") }
       assign(:categories, @categories)
     end
     
     it "displays links to each category" do
       render
-      5.times { |n| expect(rendered).to have_link("Category {n}", @categories[n]) }
+      5.times { |n| expect(rendered).to have_link("Category #{n}", @categories[n]) }
     end
   end
 
