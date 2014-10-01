b844d9c5400b4fdf4cdfd52edcfb61da44513f83
Kevin McMorrow <kmcm07+github@gmail.com>
2014-10-01 17:36:55 +0100

    Add status column to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809131901) do
+ActiveRecord::Schema.define(version: 20141001162946) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status",     default: 0
   end
 
   create_table "articles_categories", id: false, force: true do |t|
