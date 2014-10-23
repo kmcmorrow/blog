3e5f13a73e70ca3324d24ce775acaec713f95492
Kevin McMorrow <kmcm07+github@gmail.com>
2014-10-23 23:33:03 +0100

    Add helper for publish/unpublish/edit/delete links

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,16 @@
 module ArticlesHelper
+  def admin_button(article)
+    if article.published?
+      link_text = 'Unpublish'
+      button_style = 'btn-success'
+    else
+      link_text = 'Publish'
+      button_style = 'btn-danger'
+    end
+    render partial: 'articles/admin_button', locals: {
+      article: article,
+      link_text: link_text,
+      button_style: button_style
+    }
+  end
 end
